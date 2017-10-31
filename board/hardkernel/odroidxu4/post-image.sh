21b383bf7e790831c439c5ba33dbf318b12365c5
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2017-10-31 08:17:31 -0500

    Add config for odroidxu4

---
+++ board/hardkernel/odroidxu4/post-image.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+BOARD_DIR="$(dirname $0)"
+GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+UBOOT_DIR="${BUILD_DIR}/uboot-odroidxu4-v2017.05/sd_fuse"
+
+cp ${BOARD_DIR}/boot.ini ${BINARIES_DIR}/
+cp ${BUILD_DIR}/uboot-odroidxu4-v2017.05/arch/arm/dts/exynos5422-odroidxu4.dtb ${BINARIES_DIR}/
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage                           \
+	--rootpath "${TARGET_DIR}"     \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
+
+signed_bl1_position=1
+bl2_position=31
+uboot_position=63
+tzsw_position=1503
+env_position=2015
+
+dd iflag=dsync oflag=dsync conv=notrunc if="${UBOOT_DIR}/bl1.bin.hardkernel"            of="${BINARIES_DIR}/sdcard.img" seek=$signed_bl1_position
+dd iflag=dsync oflag=dsync conv=notrunc if="${UBOOT_DIR}/bl2.bin.hardkernel.720k_uboot" of="${BINARIES_DIR}/sdcard.img" seek=$bl2_position
+dd iflag=dsync oflag=dsync conv=notrunc if="${BINARIES_DIR}/u-boot-dtb.bin"             of="${BINARIES_DIR}/sdcard.img" seek=$uboot_position
+dd iflag=dsync oflag=dsync conv=notrunc if="${UBOOT_DIR}/tzsw.bin.hardkernel"           of="${BINARIES_DIR}/sdcard.img" seek=$tzsw_position
+dd iflag=dsync oflag=dsync conv=notrunc if=/dev/zero                                    of="${BINARIES_DIR}/sdcard.img" seek=$env_position count=32 bs=512
