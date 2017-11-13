ddd32505010748fdac4ef62200aa8045cec7d06e
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2017-11-13 12:37:16 -0600

    Working odroid Xu4 config

--- board/hardkernel/odroidxu4/post-image.sh
+++ board/hardkernel/odroidxu4/post-image.sh
@@ -4,15 +4,11 @@ BOARD_DIR="$(dirname $0)"
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 UBOOT_DIR="${BUILD_DIR}/uboot-odroidxu4-v2017.05/sd_fuse"
-MKIMAGE="${HOST_DIR}/bin/mkimage"
-INIT="${BINARIES_DIR}/rootfs.cpio"
-UINIT="${BINARIES_DIR}/uInitrd"
-
-
-"${MKIMAGE}" -C none -A arm -T ramdisk -n uInitrd -d "${INIT}" "${UINIT}"
 
 cp ${BOARD_DIR}/boot.ini ${BINARIES_DIR}/
 cp ${BUILD_DIR}/uboot-odroidxu4-v2017.05/arch/arm/dts/exynos5422-odroidxu4.dtb ${BINARIES_DIR}/
+
+cp  "${BINARIES_DIR}/zImage.exynos5422-odroidxu4" "${BINARIES_DIR}/zImage"
 
 rm -rf "${GENIMAGE_TMP}"
 
@@ -32,5 +28,6 @@ env_position=2015
 dd iflag=dsync oflag=dsync conv=notrunc if="${UBOOT_DIR}/bl1.bin.hardkernel"            of="${BINARIES_DIR}/sdcard.img" seek=$signed_bl1_position
 dd iflag=dsync oflag=dsync conv=notrunc if="${UBOOT_DIR}/bl2.bin.hardkernel.720k_uboot" of="${BINARIES_DIR}/sdcard.img" seek=$bl2_position
 dd iflag=dsync oflag=dsync conv=notrunc if="${BINARIES_DIR}/u-boot-dtb.bin"             of="${BINARIES_DIR}/sdcard.img" seek=$uboot_position
+#dd iflag=dsync oflag=dsync conv=notrunc if="${UBOOT_DIR}/u-boot.bin.hardkernel"             of="${BINARIES_DIR}/sdcard.img" seek=$uboot_position
 dd iflag=dsync oflag=dsync conv=notrunc if="${UBOOT_DIR}/tzsw.bin.hardkernel"           of="${BINARIES_DIR}/sdcard.img" seek=$tzsw_position
 dd iflag=dsync oflag=dsync conv=notrunc if=/dev/zero                                    of="${BINARIES_DIR}/sdcard.img" seek=$env_position count=32 bs=512
